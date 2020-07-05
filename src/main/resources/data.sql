e7c96242d473c0c35c51ec4d03edfc4ed3534c51
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-07-05 02:48:41 -0700

    feat: Application initializes with 20 cars

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,39 @@ INSERT INTO price (currency, price, vehicle_id)
 VALUES ('USD', 1000, 1);
 INSERT INTO price (currency, price, vehicle_id)
 VALUES ('USD', 2000, 2);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 3000, 3);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 4000, 4);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 5000, 5);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 6000, 6);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 7000, 7);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 8000, 8);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 9000, 9);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 10000, 10);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 11000, 11);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 12000, 12);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 13000, 13);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 14000, 14);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 15000, 15);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 16000, 16);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 17000, 17);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 18000, 18);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 19000, 19);
+INSERT INTO price (currency, price, vehicle_id)
+VALUES ('USD', 20000, 20);
